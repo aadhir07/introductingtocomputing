6092493463a288c2bc46bccdcd49f72fa3a152c2
aadhir07 <96745523+aadhir07@users.noreply.github.com>
2022-01-06 06:08:49 +0530

    Add files via upload

---
+++ 1q.py
@@ -0,0 +1,9 @@
+#program to find average of 3 numbers!!
+
+num1=float(input("Enter 1st number: "))
+num2=float(input("Enter 2nd number: "))
+num3=float(input("Enter 3rd number: "))
+
+avg=((num1+num2+num3)/3)
+
+print("The average of", num1, num2,num3, "is", avg)
